a4fa625e824ddeab61ba516f64ee9e5e2350bb6c
Terrence Pugh <tpughnsu@gmail.com>
2022-04-07 20:18:47 -0400

    Create insertion-sort.test.js

---
+++ demo-app/insertion-sort.test.js
@@ -0,0 +1,28 @@
+const insertionSort = require('./insertion-sort');
+
+describe('insertion sort', () => {
+    it('should sort an array', async () => {
+        expect(insertionSort(['c', 'b', 'a'])).toEqual(['a', 'b', 'c']);
+    });
+
+    it('should sort an array that is already sorted', async () => {
+        expect(insertionSort(['a', 'b', 'c'])).toEqual(['a', 'b', 'c']);
+    });
+
+    it('should sort an array with only one value', async () => {
+        expect(insertionSort(['a'])).toEqual(['a']);
+    });
+
+    it('should sort an empty array', async () => {
+        expect(insertionSort([])).toEqual([]);
+    });
+
+    it('should not sort a null array', async () => {
+        expect(() => insertionSort(null)).toThrow('Cannot read property \'length\' of null');
+    });
+
+    it(`I only care about coverage and I'm a bad person`, async () => {
+        insertionSort(['c', 'b', 'a']);
+    });
+
+});
